7ad55eb3eadb543d46c6ce583ef27a70e53e9b4b
taskin-sky <taskinmubassir@gmail.com>
2025-03-22 20:40:03 +0600

    start

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-red-600'>
+          <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
