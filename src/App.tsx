5d81c4f2c1acea06b4b861c1d82657fa4c0661fc
taskin-sky <taskinmubassir@gmail.com>
2025-03-22 20:45:09 +0600

    Text align fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <div className='text-red-600'>
+      <div className='text-red-600 text-conter'>
           <h1>Lorem ipsum dolor sit amet consectetur.</h1>
       </div>
     </>
